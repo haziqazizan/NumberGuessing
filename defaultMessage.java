a90bac6f02a7b48d214816f96b174eda04f93a92
haziq azizan <haziqazizan76@gmail.com>
2026-06-18 21:07:04 +0800

    delete file

--- defaultMessage.java
+++
@@ -1,8 +0,0 @@
-public abstract class defaultMessage {
-    abstract void welcoming();
-
-    public void guessNumberRule(){
-        System.out.println("I'm thinking of a number between 1 and 100.\r\n" + //
-                        "You have 5 chances to guess the correct number.");
-    }
-}
